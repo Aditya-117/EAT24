7a51697f978321a34a6bd1c60318c53fedb2c5e6
Aditya-117 <64463867+Aditya-117@users.noreply.github.com>
2023-04-29 15:22:58 +0530

    Add files via upload

---
+++ Pizza_Hut.php
@@ -0,0 +1,203 @@
+<html>
+    <head>
+        <title>Pizza Hut</title>
+        </head>
+        <body>
+
+		<img src="https://www.freepnglogos.com/uploads/pizza-hut-png-logo/the-dock-pizza-hut-png-logo-2.png" height="120" width="450" ></img>
+<center><p align="right">
+<font color="green" size="+1"><b><u>Phone Number :(+91) </u></b>8655615995</font>
+</p></center>
+<hr>      
+<form action="Cart.php" method="GET">
+<table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Margherita</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://dynl.mktgcdn.com/p/9WplRz27Yb7L7uK8ga1e1E9ucO1NX8M6juZLDMxze3s/619x619.jpg" height="100" width="100" ></img></td>
+	
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num1" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;CUSTOMIZE: </front></b></td>
+<td><select name="cust1">
+<option value="0">-- Please Select --</option>
+<option value="1">Pan Crust(+Rs0)</option>
+<option value="2">Cheese Maxx(+Rs75)</option>
+<option value="3">Kebab Crust(+Rs99)</option>
+</select></td>
+</tr>
+</table>
+<fieldset id="what1">
+<input type="radio" name="what1" value="PER"><b><font size="+1"color="green">Rs129(PER)</font></b>
+<input type="radio" name="what1" value="MED" ><b><font size="+1"color="green">Rs279(MED)</font></b>	</br>
+</fieldset>
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Corn & Cheese</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://b.zmtcdn.com/data/dish_photos/7a2/d339fe614c863538f2a61ca721d467a2.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num2" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;CUSTOMIZE: </front></b></td>
+<td><select name="cust2">
+<option value="0">-- Please Select --</option>
+<option value="1">Pan Crust(+Rs0)</option>
+<option value="2">Cheese Maxx(+Rs75)</option>
+<option value="3">Kebab Crust(+Rs99)</option>
+</select></td>
+</tr>
+</table>
+<fieldset id="what2">
+<input type="radio" name="what2" value="PER"><b><font size="+1"color="green">Rs189(PER)</font></b>
+<input type="radio" name="what2" value="MED" ><b><font size="+1"color="green">Rs379(MED)</font></b>	</br>
+</fieldset> 
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Veggie Tandoori</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/tandoori-paneer.4ef45717e972cf45b43c010e3cde5a22.1.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num3" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;CUSTOMIZE: </front></b></td>
+<td><select name="cust3">
+<option value="0">-- Please Select --</option>
+<option value="1">Pan Crust(+Rs0)</option>
+<option value="2">Cheese Maxx(+Rs75)</option>
+<option value="3">Kebab Crust(+Rs99)</option>
+</select></td>
+</tr>
+</table>
+<fieldset id="what3">
+<input type="radio" name="what3" value="PER"><b><font size="+1"color="green">Rs239(PER)</font></b>
+<input type="radio" name="what3" value="MED" ><b><font size="+1"color="green">Rs449(MED)</font></b>	</br>
+</fieldset>
+ 
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Farmer's Pick</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/farmers-pick.794695a631c14d4404057470020be703.1.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num4" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;CUSTOMIZE: </front></b></td>
+<td><select name="cust4">
+<option value="0">-- Please Select --</option>
+<option value="1">Pan Crust(+Rs0)</option>
+<option value="2">Cheese Maxx(+Rs75)</option>
+<option value="3">Kebab Crust(+Rs99)</option>
+</select></td>
+</tr>
+</table>
+<fieldset id="what4">
+<input type="radio" name="what4" value="PER"><b><font size="+1"color="green">Rs289(PER)</font></b>
+<input type="radio" name="what4" value="MED" ><b><font size="+1"color="green">Rs529(MED)</font></b>	</br>
+</fieldset>
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Country Feast</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/country-feast.e4032b4f076b2f961cac86d15af1a0f5.1.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num5" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;CUSTOMIZE: </front></b></td>
+<td><select name="cust5">
+<option value="0">-- Please Select --</option>
+<option value="1">Pan Crust(+Rs0)</option>
+<option value="2">Cheese Maxx(+Rs75)</option>
+<option value="3">Kebab Crust(+Rs99)</option>
+</select></td>
+</tr>
+</table>
+<fieldset id="what5">
+<input type="radio" name="what5" value="PER"><b><font size="+1"color="green">Rs289(PER)</font></b>
+<input type="radio" name="what5" value="MED" ><b><font size="+1"color="green">Rs529(MED)</font></b>	</br>
+</fieldset>
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Veg Kebab Surprise</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/veg-kebab-surprise.abab1dff179ab8cf95a59f30d6352297.1.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num6" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;CUSTOMIZE: </front></b></td>
+<td><select name="cust6">
+<option value="0">-- Please Select --</option>
+<option value="1">Pan Crust(+Rs0)</option>
+<option value="2">Cheese Maxx(+Rs75)</option>
+<option value="3">Kebab Crust(+Rs99)</option>
+</select></td>
+</tr>
+</table>
+<fieldset id="what6">
+<input type="radio" name="what6" value="PER"><b><font size="+1"color="green">Rs349(PER)</font></b>
+<input type="radio" name="what6" value="MED" ><b><font size="+1"color="green">Rs599(MED)</font></b>	</br>
+</fieldset>
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Zesty Paneer Pocket</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://api.pizzahut.io/v1/content/en-in/in-1/images/side/zesty-veg-pocket.ba790bc6fa8f27ea8e99e31c4edc7c48.1.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num7" min="0" value="ADD" style="height:20px;width:70px" ></td>
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs119</font></b></br>
+</fieldset>
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Masala Lemonade</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://api.pizzahut.io/v1/content/en-in/in-1/images/drink/masala-lemonade.671f99d3f05e6a9bfa620046ac5168c4.1.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num8" min="0" value="ADD" style="height:20px;width:70px" ></td>
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs119</font></b></br>
+</fieldset>
+
+
+<br>
+<br>
+<?php
+$file=fopen('temp.txt','w');
+fwrite($file,"Pizza Hut");
+fclose($file);
+?>
+<input type="submit" value="ADD TO CART" style="height:50px;width:200px;color:green">
+		</form></br>
+
+             </body>
+</html>
+
+
---
+++ Xero_Degrees.php
@@ -0,0 +1,167 @@
+<html>
+    <head>
+        <title>Xero Degrees</title>
+        </head>
+        <body>
+
+                 <img src="https://xerodegrees.in/wp-content/uploads/2021/12/xero-logo.png" height="120" width="240" ></img>
+<center><p align="right">
+<font color="green" size="+1"><b><u>Phone Number :(+91) </u></b>8753322471</font>
+</p></center>
+<hr>
+
+<form action="Cart.php" method="GET">
+<table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Super Veggie Burger</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/ng0jzubtztvxi0fbjyqm" height="100" width="100" ></img></td>
+	
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num1" min="0" value="ADD" style="height:20px;width:70px" ></td>
+
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs169</font></b></br>
+</fieldset>
+          
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>X-Veg Burger</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://b.zmtcdn.com/data/pictures/8/18716038/309fd0f92675fcb493dbc01d302d8556_featured_v2.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num2" min="0" value="ADD" style="height:20px;width:70px" ></td>
+
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs199</font></b></br>
+</fieldset>
+
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Pizza in a jar</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://res.cloudinary.com/purnesh/image/upload/f_auto/v1533209980/pizza-in-a-jar-0769.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num3" min="0" value="ADD" style="height:20px;width:70px" ></td>
+
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs179</font></b></br>
+</fieldset> 
+
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Xero Steam Dumplings</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://b.zmtcdn.com/data/reviews_photos/ce5/f527170e1c5b7ff57b3e3e4ea56e1ce5_1659790874.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num4" min="0" value="ADD" style="height:20px;width:70px" ></td>
+
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs209</font></b></br>
+</fieldset>
+
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Choco Fudge Waffle</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://imgmedia.lbb.in/media/2020/02/5e4593cfd6ac5e8aa08043ce_1581618127506.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num5" min="0" value="ADD" style="height:20px;width:70px" ></td>
+
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs189</font></b></br>
+</fieldset>
+
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Coolers</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://imgmedia.lbb.in/media/2019/05/5ced2f031095df11cd87dc54_1559047939018.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num6" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;FLAVOUR: </front></b></td>
+<td><select name="cust1">
+<option value="0">-- Please Select --</option>
+<option value="1">Lemon Ice Tea</option>
+<option value="2">Peach Ice Tea</option>
+<option value="3">Ocean Blue</option>
+<option value="4">Cucumber Mint</option>
+</select></td>
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs190</font></b></br>
+</fieldset>
+
+
+<br><table><tr>
+<td><img src="https://static.thenounproject.com/png/4084271-200.png" heigth=10 width=10></img></td>
+<td><font size="+2"><b>Shakes</b></font></td>
+</table>
+<table>
+<tr>
+<td><img src="https://b.zmtcdn.com/data/reviews_photos/ed6/8f1351d4dcc8904b8ed71f65f5991ed6_1576908726.jpg" height="100" width="100" ></img></td>
+	</br>
+	<td><b><font size="+1"color="green">&nbsp;&nbsp;ADD: </front></b></td>
+	<td><input type="number" name="num7" min="0" value="ADD" style="height:20px;width:70px" ></td>
+<td><b><font size="+1"color="green">&nbsp;&nbsp;FLAVOUR: </front></b></td>
+<td><select name="cust2">
+<option value="0">-- Please Select --</option>
+<option value="1">Choco Brownie</option>
+<option value="2">Kitkat</option>
+<option value="3">Red Velvet</option>
+<option value="4">Popcorn Caramel</option>
+</select></td>
+</tr>
+</table>
+<fieldset>
+<b><font size="+1" color="green">Rs249</font></b></br>
+</fieldset>
+
+
+<br>
+<br>
+<?php
+$file=fopen('temp.txt','w');
+fwrite($file,"Xero Degrees");
+fclose($file);
+?>
+<input type="submit" value="ADD TO CART" style="height:50px;width:200px;color:green">
+		</form></br>
+
+             </body>
+</html>
+
+
